6de70d32d50c3a1afb6f5e3de16280e6339db43c
akshay-1705 <akshay.arora1705@gmail.com>
2024-03-16 02:02:30 +0530

    move queries to a concern

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Location < ApplicationRecord
+  include LocationQueries
+
   validates :name, :zip, :country_code, :latitude, :longitude, presence: true
   has_many :air_pollution_data_points
 
@@ -12,26 +14,5 @@ class Location < ApplicationRecord
       longitude: data['lon'],
       country_code: data['country']
     )
-  end
-
-  def self.average_aqi_per_location
-    ::Location.joins(:air_pollution_data_points)
-              .group('locations.zip')
-              .average(:aqi)
-              .transform_values { |value| value.to_f.round(2) }
-  end
-
-  def self.average_aqi_per_state
-    ::Location.joins(:air_pollution_data_points)
-              .group('locations.name')
-              .average(:aqi)
-              .transform_values { |value| value.to_f.round(2) }
-  end
-
-  def self.average_aqi_per_month_per_location
-    Location.joins(:air_pollution_data_points)
-            .group('locations.zip', "EXTRACT(MONTH FROM air_pollution_data_points.received_at) || '-' || EXTRACT(YEAR FROM air_pollution_data_points.received_at)")
-            .average('air_pollution_data_points.aqi')
-            .transform_values { |value| value.to_f.round(2) }
   end
 end
